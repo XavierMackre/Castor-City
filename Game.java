2a1e55548a4fe779282c68460701f3afa41e6506
XavierMackre <xavier.mackre@insa-lyon.fr>
2015-04-21 12:26:38 +0200

    implémentation d'un "vraie" classe game

--- Game.java
+++ Game.java
@@ -1,20 +1,31 @@
+package castorcity;
+
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.Timer;
 
+
 public class Game {
-    public static void main(String[] args) {
-        Timer timer;
-        int hour=0;
-        int minute=0;
-        int day=1;
-        int month=0;
-        
-        timer = new Timer(100, new TimerAction());
+    
+    Timer timer;
+    int minute= 0;
+    int hour=0;
+    int day=1;
+    int month=1;
+    int year=2015;
+    int[] date= {minute, hour, day, month, year};
+    
+    class TimerClass implements ActionListener {
+
+                    public void actionPerformed (ActionEvent e) {
+                        horloge(date);
+
+                    }
         
     }
-    public void actionPerformed(ActionEvent e) {
-        minute+=1;
+    private void  horloge (){
+        minute +=1;
         if(minute==60){
             hour++;
             minute=0;
@@ -24,10 +35,23 @@ public class Game {
                 if(day==30){
                     day=1;
                     month++;
+                    if (month==12){
+                        month=1;
+                        year++;
+                    }
                 }
             }
         }
     }
-      
-          
+
+
+    
+    public Game {
+                   
+                   
+        timer = new Timer (100, new TimerClass());
+        timer.start();
+        
+    }
 }
+    
